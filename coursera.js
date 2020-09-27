af43672c681b945f23d96000da507126449f72fb
Victory Uzochukwu <uzochukwuv@yahoo.com>
2020-09-27 16:02:32 +0200

    3 commits

--- coursera.js
+++ coursera.js
@@ -85,6 +85,11 @@ npm install jquery@3.3.1 popper.js@1.12.9 --save
 npm start // Starts lite server
 
 npm install font-awesome@4.7.0 --save
+ http://fontawesome.io/ 
 
 npm install bootstrap-social@5.1.1 --save // social icons
+https://lipis.github.io/bootstrap-social/
+
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
  */
