eb5bddaac8fa68da4a105301cf6d51fc693211db
Victory Uzochukwu <uzochukwuv@yahoo.com>
2020-09-27 15:51:46 +0200

    Six commits today

--- coursera.js
+++ coursera.js
@@ -83,4 +83,8 @@ npm install bootstrap@4.0.0 --save
 npm install jquery@3.3.1 popper.js@1.12.9 --save
 
 npm start // Starts lite server
+
+npm install font-awesome@4.7.0 --save
+
+npm install bootstrap-social@5.1.1 --save // social icons
  */
